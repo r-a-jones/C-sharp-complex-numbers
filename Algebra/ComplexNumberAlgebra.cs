99d772b9d3344a6366049e9e74feadc6711d54dc
Robin <67515128+r-a-jones@users.noreply.github.com>
2023-04-23 11:56:07 +0100

    Line geometry, algebra: quadratic equations

---
+++ Algebra/ComplexNumberAlgebra.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ComplexNumbers.Mathematics;
+
+namespace ComplexNumbers.Algebra
+{
+	public static class ComplexNumberAlgebra
+	{
+		/// <summary>
+		/// Returns the root of the linear equation az+b=0.
+		/// </summary>
+		/// <param name="a">The coefficient of the z term.</param>
+		/// <param name="b">The coefficient of the constant term.</param>
+		/// <returns>A <c>ComplexNumber[]</c> of length 1 containing root of the linear equation, if such a root exists. An empty <c>ComplexNumber[]</c> otherwise.</returns>
+		public static ComplexNumber[] RootOfLinearEquation(ComplexNumber a, ComplexNumber b)
+		{
+			if (a == 0)
+			{
+				return new ComplexNumber[0];
+			}
+			else
+			{
+				return new ComplexNumber[] { -1 * b / a };
+			}
+		}
+		/// <summary>
+		/// Returns the discriminant of the quadratic equation az²+bz+c=0, b²-4ac.
+		/// </summary>
+		/// <param name="a">The coefficient of the z² term.</param>
+		/// <param name="b">The coefficient of the z term.</param>
+		/// <param name="c">The coefficient of the constant term.</param>
+		/// <returns>A complex number representing the discriminant of this quadratic equation.</returns>
+		public static ComplexNumber DistriminantOfQuadraticEquation(ComplexNumber a, ComplexNumber b, ComplexNumber c)
+		{
+			return b * b - 4 * a * c;
+		}
+
+		/// <summary>
+		/// Returns the roots of the quadratic equation az²+bz+c=0.
+		/// </summary>
+		/// <param name="a">The coefficient of the z² term.</param>
+		/// <param name="b">The coefficient of the z term.</param>
+		/// <param name="c">The coefficient of the constant term.</param>
+		/// <returns>The roots of the quadratic equation.</returns>
+		public static ComplexNumber[] RootsOfQuadraticEquation(ComplexNumber a, ComplexNumber b, ComplexNumber c)
+		{
+			if (a == 0) //is just a linear equation
+			{
+				return RootOfLinearEquation(b, c);
+			}
+			ComplexNumber root1 = (-1 * b + Mathematics.Math.Sqrt(DistriminantOfQuadraticEquation(a,b,c))) / (2 * a);
+			ComplexNumber root2 = (-1 * b - Mathematics.Math.Sqrt(DistriminantOfQuadraticEquation(a, b, c))) / (2 * a);
+
+			return new ComplexNumber[] { root1, root2 };
+		}
+
+		/// <summary>
+		/// Returns the discriminant of the quadratic equation az²+bz+c=0, b²-4ac.
+		/// </summary>
+		/// <param name="a">The coefficient of the z² term.</param>
+		/// <param name="b">The coefficient of the z term.</param>
+		/// <param name="c">The coefficient of the constant term.</param>
+		/// <returns>A double representing the discriminant of this quadratic equation.</returns>
+		public static double DistriminantOfQuadraticEquation(double a, double b, double c)
+		{
+			return b * b - 4 * a * c;
+		}
+
+		/// <summary>
+		/// Returns the roots of the quadratic equation az²+bz+c=0.
+		/// </summary>
+		/// <param name="a">The coefficient of the z² term.</param>
+		/// <param name="b">The coefficient of the z term.</param>
+		/// <param name="c">The coefficient of the constant term.</param>
+		/// <returns>The roots of the quadratic equation.</returns>
+		public static ComplexNumber[] RootsOfQuadraticEquation(double a, double b, double c)
+		{
+			if (a == 0) //is just a linear equation
+			{
+				return RootOfLinearEquation(b, c);
+			}
+
+			ComplexNumber root1 = (-1 * b + Mathematics.Math.Sqrt(DistriminantOfQuadraticEquation(a, b, c))) / (2 * a);
+			ComplexNumber root2 = (-1 * b - Mathematics.Math.Sqrt(DistriminantOfQuadraticEquation(a, b, c))) / (2 * a);
+
+			return new ComplexNumber[] { root1, root2 };
+		}
+	}
+}
--- Geometry/ComplexNumberGeometry.cs
+++ Geometry/ComplexNumberGeometry.cs
@@ -9,7 +9,7 @@ namespace ComplexNumbers.Geometry
 {
 	public static class ComplexNumberGeometry
 	{
-
+		#region Distance
 		public static double Distance(ComplexNumber a, ComplexNumber b)
 		{
 			return (b - a).Modulus;
@@ -18,6 +18,7 @@ namespace ComplexNumbers.Geometry
 		{
 			return (b - a).ModulusSquared;
 		}
+		#endregion
 
 		public static double AreaOfTriangleWithVertices(ComplexNumber a, ComplexNumber b, ComplexNumber c)
 		{
@@ -28,5 +29,48 @@ namespace ComplexNumbers.Geometry
 
 			return 0.5 * diff1.Modulus * diff2.Modulus * System.Math.Sin(argDiff);
 		}
+
+		#region Lines
+		/// <summary>
+		/// Returns whether the lines defined by the points (<c>line1A</c>, <c>line1B</c>) and (<c>line2A</c>, <c>line2B</c>) are parallel.
+		/// </summary>
+		/// <param name="line1A">A point on the first line.</param>
+		/// <param name="line1B">A point on the first line.</param>
+		/// <param name="line2A">A point on the second line.</param>
+		/// <param name="line2B">A point on the second line.</param>
+		/// <returns>Boolean - <c>true</c> if the lines are parallel, <c>false</c> if they are not.</returns>
+		/// <exception cref="Exception">Throws an exception if the points to define a line are not distinct.</exception>
+		public static bool LinesAreParallel(ComplexNumber line1A, ComplexNumber line1B, ComplexNumber line2A, ComplexNumber line2B)
+		{
+			if ((line1A == line1B) || (line2A == line2B))
+			{
+				throw new Exception("Points defining lines need to be distinct.");
+			}
+
+			return ((line1B - line1A).Argument == (line2B - line2A).Argument);
+		}
+
+		/// <summary>
+		/// Returns whether the lines defined by the points (<c>line1A</c>, <c>line1B</c>) and (<c>line2A</c>, <c>line2B</c>) intersect.
+		/// </summary>
+		/// <param name="line1A">A point on the first line.</param>
+		/// <param name="line1B">A point on the first line.</param>
+		/// <param name="line2A">A point on the second line.</param>
+		/// <param name="line2B">A point on the second line.</param>
+		/// <returns>Boolean - <c>true</c> if the lines intersect, <c>false</c> if they do not.</returns>
+		/// <exception cref="Exception">Throws an exception if the points to define a line are not distinct.</exception>
+
+		public static bool LinesIntersect(ComplexNumber line1A, ComplexNumber line1B, ComplexNumber line2A, ComplexNumber line2B)
+		{
+			if ((line1A == line1B) || (line2A == line2B))
+			{
+				throw new Exception("Points defining lines need to be distinct.");
+			}
+			return !(LinesAreParallel(line1A, line1B, line2A, line2B));
+		}
+		#endregion
+
+		#region Circles
+		#endregion
 	}
 }
