87f2bb35d43ef3fdff1624a2811360edf1284176
Robin <67515128+r-a-jones@users.noreply.github.com>
2023-04-23 01:20:13 +0100

    Riemann sphere

---
+++ Geometry/Riemann Sphere/ComplexNumberRiemannSphereGeometry.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ComplexNumbers.Geometry.Riemann_Sphere
+{
+	public static class ComplexNumberRiemannSphereGeometry
+	{
+		public static (double, double, double) ToRiemannSphere(ComplexNumber z)
+		{
+			double projectDenominator = 1 + z.Real * z.Real + z.Imaginary * z.Imaginary;
+			double sphereX = 2 * z.Real / projectDenominator;
+			double sphereY = 2 * z.Imaginary / projectDenominator;
+			double sphereZ = (-1 + z.Real * z.Real + z.Imaginary * z.Imaginary) / projectDenominator;
+
+			return (sphereX, sphereY, sphereZ);
+		}
+		public static (double, double, double) ToRiemannSphere(ComplexNumber z, RiemannSpherePole poleToProjectInfinityTo)
+		{
+			switch (poleToProjectInfinityTo)
+			{
+				case RiemannSpherePole.north:
+					return ToRiemannSphere(z);
+				case RiemannSpherePole.south:
+					return ToRiemannSphere(1 / z.Conjugate);
+				default:
+					throw new Exception("Could not parse RiemannSpherePole" + poleToProjectInfinityTo.ToString() + ".");
+			}
+		}
+
+		public enum RiemannSpherePole
+		{
+			north, south
+		}
+	}
+}
